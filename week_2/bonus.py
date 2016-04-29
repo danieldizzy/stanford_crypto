7eb0fc6545c6000745901a97f832261fe0462f8d
Jeff Zohrab <jzohrab@gmail.com>
2016-04-29 15:46:00 -0400

    Week 2 bonus, sort of.

--- week_2/bonus.py
+++ week_2/bonus.py
@@ -29,45 +29,44 @@ from lib.helpers import *
 
 import base64
 from Crypto.Cipher import AES
+import Crypto.Util.Counter
 from Crypto import Random
 
 import sys
 
-def decrypt(key, ct):
-    h = lambda x: hexstring_to_char_string(x)
-    key = h(key)
-    ct = h(ct)
+def decrypt_cbc(key, ct):
+    key, ct = map(hexstring_to_char_string, (key, ct))
     iv = ct[:16]
     cipher = AES.new(key, AES.MODE_CBC, iv)
+    return cipher.decrypt(ct[16:])
+
+def decrypt_ctr(key, ct):
+    key, ct = map(hexstring_to_char_string, (key, ct))
+    iv = ct[:16]
+    ctr = Crypto.Util.Counter.new(128, initial_value=long(iv.encode("hex"), 16))
+    cipher = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CTR, counter=ctr)
     return cipher.decrypt(ct[16:])
 
 
 ###################
 
-def bonus():
-    pass
-
 def main():
 
     key = '140b41b22a29beb4061bda66b6747e14'
     ct = '4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81'
-    print decrypt(key, ct)
+    print decrypt_cbc(key, ct)
 
     key = '140b41b22a29beb4061bda66b6747e14'
     ct = '5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253'
-    print decrypt(key, ct)
+    print decrypt_cbc(key, ct)
 
-    sys.exit(0)
-
-    # q3 - CTR
     key = "36f18357be4dbd77f050515c73fcf9f2"
     ct = "69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329"
-    print decrypt(key, AES.MODE_CTR, ct)
+    print decrypt_ctr(key, ct)
     
-    # q4 - CTR
     key = "36f18357be4dbd77f050515c73fcf9f2"
     ct = "770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451"
-
+    print decrypt_ctr(key, ct)
 
 if __name__ == '__main__':
     main()
